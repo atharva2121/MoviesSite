ec2419da8d330b00d0d5f3d6c40a681ee1994e8b
atharva.deshpande <atharva.deshpande@exfo.com>
2023-07-11 15:01:45 +0530

    filters added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,7 @@ export class HomeComponent {
   moviesData = this.moviesDataService.getMoviesData();
   currentPage = 1;
   totalPages = Math.ceil(this.moviesData.length / 5);
+
   constructor(private moviesDataService: MoviesDataService, private router:Router){
 
   }
@@ -31,6 +32,43 @@ export class HomeComponent {
     }
   }
 
+ 
+  onFilterChange(filter: string): void {
+    
+
+    switch (filter) {
+      case 'date':
+        this.moviesData.sort((a, b) => new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime());
+        break;
+      case 'duration':
+        this.moviesData.sort((a, b) => {
+          const durationA = this.convertDurationToMinutes(a.duration);
+          const durationB = this.convertDurationToMinutes(b.duration);
+          return durationB - durationA;
+        });
+        break;
+      case 'rating':
+        this.moviesData.sort((a, b) => b.rating - a.rating);
+        break;
+      case 'none': 
+        this.moviesData = this.moviesDataService.getMoviesData();
+        break;
+    }
+  }
+  convertDurationToMinutes(duration: string): number {
+    const parts = duration.split(' ');
+    let minutes = 0;
   
+    for (const part of parts) {
+      if (part.includes('h')) {
+        const hours = parseInt(part.replace('h', ''), 10);
+        minutes += hours * 60;
+      } else if (part.includes('m')) {
+        const mins = parseInt(part.replace('m', ''), 10);
+        minutes += mins;
+      }
+    }
   
+    return minutes;
+  }
 }
